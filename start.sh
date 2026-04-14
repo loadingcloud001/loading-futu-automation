fe603251680b56e3b30465517ce9a186446b99d8
loadingcloud001 <loadingcloud001@gmail.com>
2026-04-15 00:28:47 +0800

    fix: use custom start.sh that respects FUTU_OPEND_RSA_FILE_PATH env var (empty=disabled)

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Don't hardcode RSA path - respect the env var
+FUTU_OPEND_IP=${FUTU_OPEND_IP:-$(cat /etc/hostname)}
+FUTU_ACCOUNT_PWD_MD5=${FUTU_ACCOUNT_PWD_MD5:-$(echo -n $FUTU_ACCOUNT_PWD | md5sum | awk '{print $1}')}
+
+echo "FUTO_ACCOUNT_ID: $FUTU_ACCOUNT_ID"
+echo "FUTU_OPEND_RSA_FILE_PATH: $FUTU_OPEND_RSA_FILE_PATH"
+echo "FUTU_OPEND_IP: $FUTU_OPEND_IP"
+
+FUTU_OPEND_XML_PATH=/bin/FutuOpenD.xml
+
+echo "Replace FutuOpenD.xml"
+
+sed -i "s|<ip>.*</ip>|<ip>$FUTU_OPEND_IP</ip>|" $FUTU_OPEND_XML_PATH
+sed -i "s|<api_port>.*</api_port>|<api_port>$FUTU_OPEND_PORT</api_port>|" $FUTU_OPEND_XML_PATH
+sed -i "s|<login_account>.*</login_account>|<login_account>$FUTU_ACCOUNT_ID</login_account>|" $FUTU_OPEND_XML_PATH
+sed -i "s|<login_pwd_md5>.*</login_pwd_md5>|<login_pwd_md5>$FUTU_ACCOUNT_PWD_MD5</login_pwd_md5>|" $FUTU_OPEND_XML_PATH
+sed -i "s|<rsa_private_key>.*</rsa_private_key>|<rsa_private_key>$FUTU_OPEND_RSA_FILE_PATH</rsa_private_key>|" $FUTU_OPEND_XML_PATH
+
+if [ -n "$FUTU_OPEND_TELNET_PORT" ]; then
+    sed -i "s|<telnet_port>.*</telnet_port>|<telnet_port>$FUTU_OPEND_TELNET_PORT</telnet_port>|" $FUTU_OPEND_XML_PATH
+fi
+
+/bin/FutuOpenD
